aeb23d170c339456ed4d6dc15b187e17a9a83be0
maybe <huyabsolution@gmail.com>
2026-04-12 12:26:13 +0700

    minor fixes

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = 'https://backend-production-446d.up.railway.app/api';
 
 interface FetchOptions extends RequestInit {
   token?: string;
